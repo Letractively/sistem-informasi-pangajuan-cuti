6c35e2f48f33b8193b805e69e3d098ab02199921
gried.man1@gmail.com <gried.man1@gmail.com@3ed6e7a1-9331-c977-9ef6-f9c9f67e7092>
2012-07-10 09:04:52 +0000

    membuat entity

---
+++ trunk/SisInfPengambilanCuti/src/java/entity/HistoryCuti.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author userCS
+ */
+@Entity
+public class HistoryCuti implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String tglawalcuti;
+    private String tglakhircuti;
+    private String alasan;
+    private String status;
+    
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "id_karyawan")
+    Karyawan karyawan;
+
+    public String getAlasan() {
+        return alasan;
+    }
+
+    public void setAlasan(String alasan) {
+        this.alasan = alasan;
+    }
+
+    public Karyawan getKaryawan() {
+        return karyawan;
+    }
+
+    public void setKaryawan(Karyawan karyawan) {
+        this.karyawan = karyawan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTglakhircuti() {
+        return tglakhircuti;
+    }
+
+    public void setTglakhircuti(String tglakhircuti) {
+        this.tglakhircuti = tglakhircuti;
+    }
+
+    public String getTglawalcuti() {
+        return tglawalcuti;
+    }
+
+    public void setTglawalcuti(String tglawalcuti) {
+        this.tglawalcuti = tglawalcuti;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof HistoryCuti)) {
+            return false;
+        }
+        HistoryCuti other = (HistoryCuti) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.HistoryCuti[id=" + id + "]";
+    }
+}
