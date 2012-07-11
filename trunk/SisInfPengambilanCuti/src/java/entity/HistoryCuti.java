3a3fd755e113f1c4b76f4c689001d0f49e3162ab
nighthawk97@gmail.com <nighthawk97@gmail.com@3ed6e7a1-9331-c977-9ef6-f9c9f67e7092>
2012-07-11 10:18:15 +0000

    Ubah generate id identity dari AUTO ke IDENTITY Karyawan DAO

--- trunk/SisInfPengambilanCuti/src/java/entity/HistoryCuti.java
+++ trunk/SisInfPengambilanCuti/src/java/entity/HistoryCuti.java
@@ -22,7 +22,7 @@ public class HistoryCuti implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tglawalcuti;
     private String tglakhircuti;
--- trunk/SisInfPengambilanCuti/src/java/entity/LoginKaryawan.java
+++ trunk/SisInfPengambilanCuti/src/java/entity/LoginKaryawan.java
@@ -22,7 +22,7 @@ import javax.persistence.OneToOne;
 public class LoginKaryawan implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
