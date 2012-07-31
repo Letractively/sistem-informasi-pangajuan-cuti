fccd50f717238608acee0da797b3b14866122fbe
nighthawk97 <nighthawk97@gmail.com>
2012-07-31 19:55:06 +0000

    Folder yang benar dari Karyawan DAO

--- SisInfPengambilanCuti/src/java/dao/KaryawanDAO.java
+++ SisInfPengambilanCuti/src/java/dao/KaryawanDAO.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dao;
 
+import entity.HistoryCuti;
 import entity.Karyawan;
 import java.util.List;
 
@@ -12,7 +12,18 @@ import java.util.List;
  *
  * @author Smart
  */
-public interface KaryawanDAO {
-     public List<Karyawan> gets() throws Exception;
+public interface KaryawanDAO extends GeneralDAO {
+
+    public List<Karyawan> gets() throws Exception;
+
     public Karyawan get(long id) throws Exception;
+
+    public List<Karyawan> getStatusKerja(String kerja) throws Exception;
+
+    public void updateSisaCuti(Long id, String cuti) throws Exception;
+
+    public void updateNotifikasi(Long id, String notif) throws Exception;
+
+    public boolean updateNotifikasi(String usr, String notif) throws Exception;
+
 }
